3a2d3e5faba226ccd7f18da6f10548807e5c552c
Lital Dvora Buchris Barak <barakli8@biu.ac.il>
2023-02-12 15:35:42 +0200

    did hist of frqq and not density

--- FCA/split_object.R
+++ FCA/split_object.R
@@ -27,12 +27,25 @@ new<-SplitObject(SERUAT_HEAD, split.by = "identity")
 gene_name <-"NPF"
 FCA1<-new$FCA1
 
+FCA1<-new$FCA1
+FCA4<-new$FCA4
+FCA5<-new$FCA5
+FCA6<-new$FCA6
+FCA7<-new$FCA7
+FCA8<-new$FCA8
+FCA9<-new$FCA9
+FCA10<-new$FCA10
+FCA11<-new$FCA11
+FCA12<-new$FCA12
+FCA13<-new$FCA13
+FCA14<-new$FCA14
+FCA15<-new$FCA15
 
 
 
 all.genes_head<-as.data.frame(rownames(SERUAT_HEAD))
 
-head_FCA<-function(gene_name,y_value,FCA){
+head_FCA<-function(gene_name,y_value,x_value,FCA,NUMBER_OF_FCA){
   
   precnt_name<-paste("percent.",gene_name,sep="")
   FCA[[precnt_name]] <- PercentageFeatureSet(FCA,features =gene_name)
@@ -41,18 +54,19 @@ head_FCA<-function(gene_name,y_value,FCA){
   colnames(gene_head)<-"head"
 
   
-  gene_head_density <- hist(gene_head$head, breaks=200,plot = FALSE)         # Store output of hist function
-  gene_head_density$density <- gene_head_density$counts /    # Compute density values
-    sum(gene_head_density$counts) * 100
+  gene_head_density <- hist(gene_head$head, breaks=seq(0,10,l=1000),plot = FALSE)         # Store output of hist function
+  #gene_head_density$density <- gene_head_density$counts /    # Compute density values
+   # sum(gene_head_density$counts) * 100
   
   
 
   
   
-  main_title <-paste("head (red) of FCA1 of ",gene_name)
+  main_title <-paste("head (red) of " ,NUMBER_OF_FCA ,gene_name)
   
-  plot(gene_head_density, freq = TRUE,col=alpha('red',0.5), main=main_title, xlab='%',ylim =c(0,y_value)) 
+  plot(gene_head_density, freq = TRUE,col=alpha('red',0.5), main=main_title, xlab='%',ylim =c(0,y_value),xlim = c(0,x_value))
+  
 }
 
-head_FCA("NPF",2,FCA1)
+head_FCA("trh",20,0.73,FCA15,"FCA15 ")
 
